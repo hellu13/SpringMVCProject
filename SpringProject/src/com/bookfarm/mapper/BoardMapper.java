59ebabf48f58c9a48dc5cfaf79a79e82125db6d5
hellu <hellu@DESKTOP-FL7IBK9>
2021-02-01 06:55:54 +0900

    글 수정 처리 구현

--- SpringProject/src/com/bookfarm/mapper/BoardMapper.java
+++ SpringProject/src/com/bookfarm/mapper/BoardMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
 
 import com.bookfarm.beans.ContentBean;
 
@@ -35,5 +36,10 @@ public interface BoardMapper {
 			+ "from content_table a1, user_table a2 "
 			+ "where a1.content_writer_idx = a2.user_idx and content_idx = #{content_idx}")
 	ContentBean getContentInfo(int content_idx);
+	
+	@Update("update content_table " +
+			"set content_subject = #{content_subject}, content_text = #{content_text}, content_file = #{content_file, jdbcType=VARCHAR} " +
+			"where content_idx = #{content_idx}")
+	void modifyContentInfo(ContentBean modifyContentBean);
 
 }
