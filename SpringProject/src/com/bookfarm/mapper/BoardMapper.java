b8e331b1561e90ba8c92798d8b703400c0708125
hellu13 <hellu13@naver.com>
2021-02-02 06:42:30 +0900

    목록보기, 수정하기 버튼 경로 설정

--- SpringProject/src/com/bookfarm/mapper/BoardMapper.java
+++ SpringProject/src/com/bookfarm/mapper/BoardMapper.java
@@ -26,7 +26,7 @@ public interface BoardMapper {
 	String getBoardInfoName(int board_info_idx);
 	
 	
-	@Select("select a1.content_idx, a1.content_subject, a2.user_name as content_wirter_name, to_char(a1.content_date, 'YYYY-MM-DD') as content_date " +
+	@Select("select a1.content_idx, a1.content_subject, a2.user_name as content_writer_name, to_char(a1.content_date, 'YYYY-MM-DD') as content_date " +
 			"from content_table a1, user_table a2 " +
 			"where a1.content_writer_idx = a2.user_idx and a1.content_board_idx = #{board_info_idx} " +
 			"order by a1.content_idx desc")
